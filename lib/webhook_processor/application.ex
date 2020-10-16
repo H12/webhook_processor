1a233ac7df4199098892da745bd8996143ff7954
Henry Firth <henrynf12@gmail.com>
2020-10-16 09:01:54 -0500

    Add endpoint and config

--- lib/webhook_processor/application.ex
+++ lib/webhook_processor/application.ex
@@ -1,6 +1,4 @@
 defmodule WebhookProcessor.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -8,12 +6,13 @@ defmodule WebhookProcessor.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: WebhookProcessor.Worker.start_link(arg)
-      # {WebhookProcessor.Worker, arg}
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: WebhookProcessor.Endpoint,
+        options: [port: Application.get_env(:webhook_processor, :port)]
+      )
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: WebhookProcessor.Supervisor]
     Supervisor.start_link(children, opts)
   end
